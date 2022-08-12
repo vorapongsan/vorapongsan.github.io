1c3b720016104ac84a20acfd53ad8c8e5bbdafce
vorapongsan <vorapong.san@mahidol.ac.th>
2022-08-12 15:34:32 +0000

    test add nuxt

---
+++ .nuxt/dist/server/pages/index.js
@@ -0,0 +1,49 @@
+exports.ids = [1];
+exports.modules = {
+
+/***/ 18:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=81a214dc&
+var render = function render() {
+  var _vm = this,
+      _c = _vm._self._c;
+
+  return _c('h1', [_vm._ssrNode("Hello world!")]);
+};
+
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=81a214dc&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/index.vue
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "109bec02"
+  
+)
+
+/* harmony default export */ var pages = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
